1d2e4c435ef9721fec2d77d1efe383249c679ca5
KIKonstantin <kikonstantin@outlook.com>
2022-11-23 19:56:41 +0200

    auth components

--- 01. Intro to Angular and TypeScript/angular demo/my-first-project/src/app/app.module.ts
+++ 01. Intro to Angular and TypeScript/angular demo/my-first-project/src/app/app.module.ts
@@ -2,34 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
-
+import { AuthModule } from './auth/auth.module';
 
 export class MyClass {
-  constructor(){
+  constructor() {
     console.log('Nameless call was constructed');
   }
 };
 
-export const myCustomToken = new InjectionToken('Test');
 
-const myProvider: Provider = {
-  provide: myCustomToken,
-  useClass: MyClass,
-};
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule
   ],
   providers: [
     MyClass,
-    myProvider
   ],
   bootstrap: [AppComponent]
 })
